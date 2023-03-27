7cd4f8a17bd9624db4fee4b75752f040492b04a2
JohnyDevelopment <78961113+JohnyDevelopment@users.noreply.github.com>
2023-03-27 09:42:04 +0200

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -4,15 +4,14 @@ RegisterCommand("policemenu", function(source, args)
     SetDisplay(not display)
 end)
 
---very important cb 
+
 RegisterNUICallback("exit", function(data)
     ESX.ShowNotification('Wyszedles z menu')
     SetDisplay(false)
     SetNuiFocus(false, false)
 end)
 
--- this cb is used as the main route to transfer data back 
--- and also where we hanld the data sent from js
+
 RegisterNUICallback("main", function(data)
     TriggerServerEvent("jhn_policemenu:custom", data.text)
     SetDisplay(false)
@@ -39,12 +38,6 @@ end
 Citizen.CreateThread(function()
     while display do
         Citizen.Wait(0)
-        -- https://runtime.fivem.net/doc/natives/#_0xFE99B66D079CF6BC
-        --[[ 
-            inputGroup -- integer , 
-	        control --integer , 
-            disable -- boolean 
-        ]]
         DisableControlAction(0, 1, display) -- LookLeftRight
         DisableControlAction(0, 2, display) -- LookUpDown
         DisableControlAction(0, 142, display) -- MeleeAttackAlternate
